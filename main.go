0d497066ed382c0fff94a9e3953bd9b48afb4ca5
Edward Buckle <edward.buckle0@gmail.com>
2019-12-04 11:55:18 +0000

    Initial CLI implementation
    Signed-off-by: Edward Buckle <edward.buckle0@gmail.com>

--- main.go
+++ main.go
@@ -1,37 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"github.com/pkg/browser"
-
-	"github.com/ebuckle/dependency-insights/insights"
-)
+import "github.com/ebuckle/dependency-insights/actions"
 
 func main() {
-	args := os.Args
-
-	projectPath := args[1]
-	projectLanguage := args[2]
-
-	response, err := insights.ProduceInsights(projectLanguage, projectPath)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
-	print, err := json.MarshalIndent(response, "", "\t")
-
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
-	_ = ioutil.WriteFile("output.json", print, 0644)
-	root, _ := os.Getwd()
-	browser.OpenURL("file:///" + root + "/output.json")
-	os.Exit(0)
+	actions.Commands()
 }
